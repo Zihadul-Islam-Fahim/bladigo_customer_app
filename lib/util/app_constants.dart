a79d4d67d199a2441b6a2cf43a399f7dd3781719
Zihadul-Islam-Fahim <fahimzihad@gmail.com>
2025-07-11 22:27:43 +0600

    Base url chagned to AWS

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -12,8 +12,8 @@ class AppConstants {
   static const String webHostedUrl = 'https://bladigo.com';
   static const bool useReactWebsite = false;
 
-  static const String baseUrl = 'https://admin.bladigo.com';
-  // static const String baseUrl = 'https://ops-console.bladigo.com';
+  // static const String baseUrl = 'https://admin.bladigo.com';
+  static const String baseUrl = 'https://ops-console.bladigo.com';
   static const String categoryUri = '/api/v1/categories';
   static const String bannerUri = '/api/v1/banners';
   static const String restaurantProductUri = '/api/v1/products/latest';
