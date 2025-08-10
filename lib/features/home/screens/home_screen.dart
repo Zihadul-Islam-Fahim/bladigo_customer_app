3d08588f3940954b920d5964975673df81a007df
Zihadul-Islam-Fahim <fahimzihad@gmail.com>
2025-08-10 10:11:59 +0600

    home page search bar desgined as previous

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -589,9 +589,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 padding: const EdgeInsets.symmetric(
                     horizontal: Dimensions.paddingSizeDefault),
                 decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.18),
+                  color: Colors.transparent,
                   borderRadius: BorderRadius.circular(25),
-                //  border: Border.all(color: Theme.of(context).primaryColor,)
+                 border: Border.all(color: Theme.of(context).primaryColor,)
                 ),
                 child: Padding(
                   padding: const EdgeInsets.only(
